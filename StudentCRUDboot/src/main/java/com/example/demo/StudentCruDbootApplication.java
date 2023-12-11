99631f67329a572be9e90bf6d4df7665719aa808
Animaw <animaw.kerie@kue.edu.et>
2023-12-11 14:38:59 +0300

    first commit

--- StudentCRUDboot/src/main/java/com/example/demo/StudentCruDbootApplication.java
+++ StudentCRUDboot/src/main/java/com/example/demo/StudentCruDbootApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StudentCruDbootApplication {
+class StudentCruDbootApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StudentCruDbootApplication.class, args);
